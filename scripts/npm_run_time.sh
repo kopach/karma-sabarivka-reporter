c06eca210dd0c89b12ad33b542a07467179dfe19
Ihor Kopach <kopach@users.noreply.github.com>
2020-06-23 14:50:26 +0200

    chore(travis): fix travis to fail on single step failure

---
+++ scripts/npm_run_time.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+script_name=$1
+
+startTime=$(date +%s)
+
+cmd="npm run ${script_name}"
+$cmd
+cmdExitCode=$(echo $? |  bc -l)
+
+if [ $cmdExitCode -ne 0 ]; then
+    exit $?
+else
+    duration=$(echo "$(date +%s) - $startTime" | bc)
+    echo "${script_name} time: ${duration}s"
+    echo "${duration}"
+fi
