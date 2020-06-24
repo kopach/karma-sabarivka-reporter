85c450c140bc0d34fda2389ca284e1e189c98404
Ihor Kopach <kopach@users.noreply.github.com>
2020-06-24 18:38:34 +0200

    refactor(*): use mocha config file, refactor seriesci script

--- scripts/npm_run_time.sh
+++ scripts/npm_run_time.sh
@@ -8,10 +8,10 @@ cmd="npm run ${script_name}"
 $cmd
 cmdExitCode=$(echo $? |  bc -l)
 
-if [ $cmdExitCode -ne 0 ]; then
-    exit ${cmdExitCode}
+if [ "${cmdExitCode}" -ne 0 ]; then
+    exit "${cmdExitCode}"
 else
     duration=$(echo "$(date +%s) - $startTime" | bc)
     echo "${script_name} time: ${duration}s"
-    echo "${duration}s" > ${script_name}-execution-time.log
+    echo "${duration}s" > "${script_name}-execution-time.log"
 fi
