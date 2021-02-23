3a278b324ec31f970ed6cb9c8d74d2eda09f10dc
Ihor Kopach <kopach@users.noreply.github.com>
2021-02-23 20:53:55 +0100

    build(scripts): 📦️ refactor scripts, fix linter issues

--- scripts/seriesci.sh
+++ scripts/seriesci.sh
@@ -8,7 +8,7 @@ SERIESCI_TOKEN=$1
 
 # More info on this: https://github.community/t/github-sha-not-the-same-as-the-triggering-commit/18286
 if [ "$GITHUB_EVENT_NAME" == "pull_request" ]; then
-    GIT_SHA=$(cat $GITHUB_EVENT_PATH | jq -r .pull_request.head.sha)
+    GIT_SHA="$(cat $GITHUB_EVENT_PATH | jq -r .pull_request.head.sha)"
 else
     GIT_SHA=$GITHUB_SHA
 fi
@@ -20,13 +20,13 @@ echo "GIT_SHA: ${GIT_SHA}"
 
 echo "post code coverage to seriesci"
 COVERAGE="$(npx lcov-total coverage/lcov.info)%"
-echo $COVERAGE
-./scripts/seriesci-post-1.sh $SERIESCI_TOKEN $GIT_SHA "coverage" $COVERAGE
+echo "${COVERAGE}"
+./scripts/seriesci-post-1.sh "${SERIESCI_TOKEN}" "${GIT_SHA}" "coverage" "${COVERAGE}"
 
 echo "post bundle size to seriesci"
 BUNDLE_SIZE="$(du -sh dist/ | awk '{print $1}')"
-echo $BUNDLE_SIZE
-./scripts/seriesci-post-1.sh $SERIESCI_TOKEN $GIT_SHA "bundlesize" $BUNDLE_SIZE
+echo "${BUNDLE_SIZE}"
+./scripts/seriesci-post-1.sh "${SERIESCI_TOKEN}" "${GIT_SHA}" "bundlesize" "${BUNDLE_SIZE}"
 
 echo "post lint & build & test execution time to seriesci"
 duration_build=$(cat build-execution-time.log)
@@ -44,11 +44,11 @@ curl \
 echo ""
 
 echo "post number of dependencies to seriesci"
-NUMBER_OF_DEPENDENCIES="$((npm ls --depth=0 --prod --parseable --silent || true) | grep -c node_modules)"
-echo $NUMBER_OF_DEPENDENCIES
-./scripts/seriesci-post-1.sh $SERIESCI_TOKEN $GIT_SHA "dependencies" "${NUMBER_OF_DEPENDENCIES}"
+NUMBER_OF_DEPENDENCIES="$( (npm ls --depth=0 --prod --parseable --silent || true) | grep -c node_modules )"
+echo "${NUMBER_OF_DEPENDENCIES}"
+./scripts/seriesci-post-1.sh "${SERIESCI_TOKEN}" "${GIT_SHA}" "dependencies" "${NUMBER_OF_DEPENDENCIES}"
 
 echo "post number of lines of code to seriesci"
 NUMBER_OF_LINES="$(npx cloc src --csv | grep 'SUM.*' -o | cut -d, -f 4)"
-echo $NUMBER_OF_LINES
-./scripts/seriesci-post-1.sh $SERIESCI_TOKEN $GIT_SHA "lines-of-code" $NUMBER_OF_LINES
+echo "${NUMBER_OF_LINES}"
+./scripts/seriesci-post-1.sh "${SERIESCI_TOKEN}" "${GIT_SHA}" "lines-of-code" $NUMBER_OF_LINES
